c8246b66bdab3b0e5a5e9a4b3d1f14619c78e751
nitin-das <nitin.das@thomson.co.uk>
2019-11-30 13:59:55 +0000

    Added new code

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "name_prefix" {
+  description = "The name of the role policy."
+}
+
+variable "policy" {
+  description = "The policy document."
+}
+
+variable "role" {
+  description = "The IAM role to attach to the policy."
+}
